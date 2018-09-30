6e00024a50468d21aca58f4c7562b4942c80501f
Uziel <uziel.perez1595@gmail.com>
2018-09-29 20:34:15 -0600

    a

--- EVA1_EXAMEN_PRACTICO_PROBLEMA_1/app/src/main/java/com/example/uziel/eva1_examen_practico_problema_1/BtnCalcular.java
+++ EVA1_EXAMEN_PRACTICO_PROBLEMA_1/app/src/main/java/com/example/uziel/eva1_examen_practico_problema_1/BtnCalcular.java
@@ -2,9 +2,11 @@ package com.example.uziel.eva1_examen_practico_problema_1;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class BtnCalcular implements View.OnClickListener {
     String sCal,sResultado;
+    TextView resultado;
     int Radio,grados;
     @Override
     public void onClick(View v) {
@@ -13,10 +15,13 @@ public class BtnCalcular implements View.OnClickListener {
             res=(4*3.14159*(Radio*Radio*Radio))/3;
         }else{
             if(sCal.equals("ar")){
-                res=4*3.14159*Radio;
+                res=4*3.14159*Radio*Radio;
             }else{
                 res=(4/3)*((3.14159*(Radio*Radio*Radio))/360)*grados;
             }
         }
+        res = (double)Math.round(res * 100d) / 100d;
+        sResultado = res+"";
+        resultado.setText(sResultado);
     }
 }
--- EVA1_EXAMEN_PRACTICO_PROBLEMA_1/app/src/main/java/com/example/uziel/eva1_examen_practico_problema_1/MainActivity.java
+++ EVA1_EXAMEN_PRACTICO_PROBLEMA_1/app/src/main/java/com/example/uziel/eva1_examen_practico_problema_1/MainActivity.java
@@ -1,23 +1,28 @@
 package com.example.uziel.eva1_examen_practico_problema_1;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
-public class MainActivity extends AppCompatActivity  implements RadioGroup.OnCheckedChangeListener,EditText.OnKeyListener{
+public class MainActivity extends AppCompatActivity  implements RadioGroup.OnCheckedChangeListener,EditText.OnEditorActionListener{
 
     EditText editTxtRadio,editTxtGrados;
     RadioGroup rdGrpCalculo;
     TextView txtResultado,txtTipoDato,txtGrados;
     Button btnCalcular,btnCerrar;
+    Button btnCalcular1;
     BtnCalcular click;
+    BtnCerrar clickCerrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,16 +39,21 @@ public class MainActivity extends AppCompatActivity  implements RadioGroup.OnChe
         btnCerrar = findViewById(R.id.btnCerrar);
 
         click = new BtnCalcular();
+        clickCerrar = new BtnCerrar();
+        btnCerrar.setOnClickListener(clickCerrar);
         click.sCal = "vol";
-        editTxtRadio.setOnKeyListener(this);
-        editTxtGrados.setOnKeyListener(new View.OnKeyListener(){
+        click.resultado = txtResultado;
+        btnCalcular.setOnClickListener(click);
+        rdGrpCalculo.setOnCheckedChangeListener(this);
+        editTxtRadio.setOnEditorActionListener(this);
+        editTxtGrados.setOnEditorActionListener(new EditText.OnEditorActionListener(){
             @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 click.grados = Integer.parseInt(editTxtGrados.getText().toString());
+                click.Radio = Integer.parseInt(editTxtRadio.getText().toString());
                 return false;
             }
         });
-
     }
 
     @Override
@@ -71,8 +81,10 @@ public class MainActivity extends AppCompatActivity  implements RadioGroup.OnChe
     }
 
     @Override
-    public boolean onKey(View v, int keyCode, KeyEvent event) {
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
         click.Radio = Integer.parseInt(editTxtRadio.getText().toString());
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(editTxtRadio.getWindowToken(), 0);
         return false;
     }
 }
